570faf5c52a8cb47c9231829f04fedb1b98e91e6
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-16 22:17:31 +0000

    “update”

--- Func_1.c
+++ Func_1.c
@@ -84,5 +84,6 @@ void my_pint(stack_t **stack, unsigned int line_number)
     else
     {
         fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
     }
 }
