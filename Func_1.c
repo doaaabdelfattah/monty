ec5e19f8a028d24ad7dd882bc3a6ac2bd7b7745e
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2023-11-16 20:58:55 +0000

    “update”

--- Func_1.c
+++ Func_1.c
@@ -8,18 +8,37 @@
 void my_push (stack_t **stack,  unsigned int line_number)
 {
     stack_t *new_node;
+    int i;
+    i = 0;
     new_node = (stack_t *)malloc(sizeof(stack_t));
     if (new_node == NULL)
     {
         err_malloc();
         return;
     }
+    /* Check if the number is valid*/
      if (opcode[1] == NULL)
     {
         err_int(line_number);
+        free(new_node);
         return;
     }
-
+    while(opcode[1][i])
+    {
+        if (opcode[1][0] == '-')
+        {
+            i++;
+            continue;
+        }
+        if (opcode[1][i] < '0' || opcode[1][i] > '9')
+        {
+        err_int(line_number);
+        free(new_node);
+        return;
+        }
+        i++;
+    }
+    
     new_node->n = atoi(opcode[1]);
     new_node->prev = *stack;
     new_node->next = NULL;
